dde11c77d1ee329826271bc5d3be316ec534e006
tobexint <tobexint@gmail.com>
2023-08-10 16:17:41 +0000

    added performance rules

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
 	   path: path.resolve(__dirname, 'public'),
 	   filename: 'bundle.js',
 	},
+	performance: {
+		maxAssetSize: 1000000
+		maxEntrypointSize: 1000000
 	module: {
 	  rules: [
 	    ...
