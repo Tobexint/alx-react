37e23e90a58a8e4c25426e086771772ddf746cb3
tobexint <tobexint@gmail.com>
2023-08-11 09:04:00 +0000

    webpack config

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	mode: 'production',
+	entry: {
+	    main: path.resolve(__dirname, './js/dashboard_main.js'),
+	   },
+	output: {
+	   path: path.resolve(__dirname, 'public'),
+	   filename: 'bundle.js',
+	},
+	performance: {
+		hints: false,
+		maxAssetSize: 1000000,
+		maxEntrypointSize: 1000000,
+	},
+	module: {
+	  rules: [
+	    ...
+	    {
+	      test: /\.css$/i,
+	      use: ["style-loader", "css-loader"]
+	    },
+	    {
+	      test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+	      type: 'asset/resource',
+	      use: [
+		'file-loader',
+		     {
+			loader: 'image-webpack-loader',
+			options: {
+				 bypassOndebug: true,
+				 disable: true,
+			 },
+		     },
+	      ]
+	    },
+	  ]
+	},
+};
