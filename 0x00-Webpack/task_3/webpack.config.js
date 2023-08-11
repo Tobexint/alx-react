fe62a10f0f7a8456b2bb2b81bd8ee09b757c7bf8
tobexint <tobexint@gmail.com>
2023-08-11 09:54:31 +0000

    Modified webpack config to support three different entry points

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
