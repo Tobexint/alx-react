5e5e1e0a4c01f1a1e188f431e6146a9429d117f6
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-08-11 02:07:01 -0700

    Modify the Webpack config to setup a development server running on the port 8564.

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,42 +1,69 @@
-const path = require('path');
+const path = require("path");
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	mode: 'production',
+  mode: 'development',
+  devtool: 'inline-source-map',
+	mode: 'development',
 	entry: {
-	    main: path.resolve(__dirname, './js/dashboard_main.js'),
-	   },
-	output: {
-	   path: path.resolve(__dirname, 'public'),
-	   filename: 'bundle.js',
+		header: {
+			import: './modules/header/header.js',
+			dependOn: 'shared',
+		},
+		body: {
+			import: './modules/body/body.js',
+			dependOn: 'shared',
+		},
+		footer: {
+			import: './modules/footer/footer.js',
+			dependOn: 'shared',
+		},
+		shared: 'jquery',
 	},
-	performance: {
-		hints: false,
+  plugins: [
+		new HTMLWebpackPlugin({
+			filename: './index.html',
+		}),
+		new CleanWebpackPlugin(),
+	],
+  optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    static: path.join(__dirname, './public'),
+    open: true,
+    port: 8564,
+  },
+  performance: {
 		maxAssetSize: 1000000,
-		maxEntrypointSize: 1000000,
 	},
-	module: {
-	  rules: [
-	    ...
-	    {
-	      test: /\.css$/i,
-	      use: ["style-loader", "css-loader"]
-	    },
-	    {
-	      test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-	      type: 'asset/resource',
-	      use: [
-		'file-loader',
-		     {
-			loader: 'image-webpack-loader',
-			options: {
-				 bypassOndebug: true,
-				 disable: true,
-			 },
-		     },
-	      ]
-	    },
-	  ]
-	},
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        use: [
+          'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true,
+							disable: true,
+						},
+					},
+				],
+      },
+    ]
+  }
 };
